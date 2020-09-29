c0edbdcd8a7372d18fc6637b53ff49246b97087b
CodeMoshete <codemoshete@gmail.com>
2020-09-28 23:43:47 -0700

    Add initial project setup

---
+++ MajorScales/Assets/Source/Engine.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Engine : MonoBehaviour
+{
+    private readonly List<int> ACTIVE_INDICES = 
+        new List<int>(new int[] { 0, 2, 4, 5, 7, 9, 11 });
+
+
+    private readonly List<string> NOTE_LABELS = new List<string>(
+        new string[] { "1", "9", "3", null, "5", null, "7" });
+
+    public List<WheelPart> WheelParts;
+    public List<Text> NoteLabels;
+    public Text KeyOfText;
+
+    private void Start()
+    {
+        SetKeyIndex(9);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void SetKeyIndex(WheelPart key)
+    {
+        SetKeyIndex(WheelParts.IndexOf(key));
+    }
+
+    private void SetKeyIndex(int index)
+    {
+        KeyOfText.text = WheelParts[index].Key;
+        int currentIndex = index;
+        int currentNoteIndex = 0;
+        for (int i = 0, count = WheelParts.Count; i < count; ++i)
+        {
+            bool isActive = ACTIVE_INDICES.Contains(i);
+            WheelPart currentWheelpart = WheelParts[currentIndex];
+            currentWheelpart.SetIsKey(i == 0);
+            currentWheelpart.SetActive(isActive);
+
+            Text currentNoteLabel = NoteLabels[currentIndex];
+            currentNoteLabel.text = string.Empty;
+            if (isActive)
+            {
+                if (NOTE_LABELS[currentNoteIndex] != null)
+                {
+                    currentNoteLabel.text = NOTE_LABELS[currentNoteIndex];
+                }
+                ++currentNoteIndex;
+            }
+
+            currentIndex = currentIndex + 1 >= count ? 0 : currentIndex + 1;
+        }
+    }
+}
---
+++ MajorScales/Assets/Source/WheelPart.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Utils;
+
+public class WheelPart : MonoBehaviour
+{
+    public string Key;
+    private GameObject onImage;
+    private GameObject offImage;
+    private GameObject keyIndicator;
+    private Button selectButton;
+    private Engine engine;
+
+    public void Awake()
+    {
+        onImage = UnityUtils.FindGameObject(gameObject, "On");
+        offImage = UnityUtils.FindGameObject(gameObject, "Off");
+        keyIndicator = UnityUtils.FindGameObject(gameObject, "KeyIndicator");
+        selectButton = GetComponentInChildren<Button>();
+        selectButton.onClick.AddListener(OnKeyButtonPressed);
+        engine = GameObject.Find("Engine").GetComponent<Engine>();
+    }
+
+    private void OnKeyButtonPressed()
+    {
+        engine.SetKeyIndex(this);
+    }
+
+    public void SetActive(bool isActive)
+    {
+        onImage.SetActive(isActive);
+        offImage.SetActive(!isActive);
+    }
+
+    public void SetIsKey(bool isKey)
+    {
+        keyIndicator.SetActive(isKey);
+    }
+}
